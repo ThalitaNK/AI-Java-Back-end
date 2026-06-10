7c359a5dfbd902832d592101b7c9ee3e0aceef95
ThalitaNK <thalita_meira@hotmail.com>
2026-06-10 17:08:58 -0300

    feat: add VerificadorDeHardware class with temperature alert functionality

---
+++ HELLO/src/VerificadorDeHardware.java
@@ -0,0 +1,23 @@
+public class VerificadorDeHardware {
+    
+    public static void main(String[] args) {
+        
+        // 1. As variáveis originais no "main"
+        String modeloProcessador = "Ryzen 5 5500";
+        int temperaturaAtual = 95;
+
+        // 2. O envio da caixa (Passando o bastão)
+        exibirAlerta(modeloProcessador, temperaturaAtual);
+    }
+
+    // 3. A porta de entrada do método recebendo a caixa
+    public static void exibirAlerta(String modelo, int temperatura) {
+        
+        // 4. O método resolvendo o problema sozinho com o que recebeu
+        if (temperatura >= 90) { 
+            System.out.println("CUIDADO! O " + modelo + " está com a temperatura " + temperatura + " ! Vai fritar!");
+        } else {
+            System.out.println("Temperatura estável. Pode continuar jogando Sea of Stars em paz.");
+        }
+    }
+}
